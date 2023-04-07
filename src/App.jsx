1f833081a0fa0eaec5a07a4ed4678685e7baa505
Felipe Roberto Rocha <87021275+Jirayakbc@users.noreply.github.com>
2023-04-07 11:06:07 -0300

    inicio das configurações

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import NotFound from "./Routes/NotFound";
 function App() {
   return (
     <>
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação teste
+      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar a classe dark ou light */}
       <div className={`{app light}`}>
         <Navbar />
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,7 @@
 import styles from "./Card.module.css";
 
-const Card = () => {
-
+const Card = (props) => {
+  const {nome, sobrenome} = props.dados;
   return (
     <>
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
@@ -16,7 +16,7 @@ const Card = () => {
           {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
           que vem da API */}
           <a href={`/dentist/MatriculaDoDentista`}>
-            <h5 className={`card-title ${styles.title}`}>Nome e Sobrenome do dentista</h5>
+            <h5 className={`card-title ${styles.title}`}>{nome} {sobrenome}</h5>
           </a>
         </div>
       </div>
