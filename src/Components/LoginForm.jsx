db526c1705cc7da530d8d0336613ced936f9b823
Jirayakbc <feliperobertorocha@gmail.com>
2023-04-08 17:51:36 -0300

    validacao formulario

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,14 +1,32 @@
 import styles from "./Form.module.css";
+import api from "../services/api"
+import { useState } from "react";
+import { async } from "q";
+import { useNavigate } from "react-router";
 
 const LoginForm = () => {
-  const handleSubmit = (e) => {
-    //Nesse handlesubmit você deverá usar o preventDefault,
+  const [username, setUsername] = useState("");
+  const [password, setPassorwd] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    //Nesse handlesubmit você deverá usar o preventDefault,-ok
     //enviar os dados do formulário e enviá-los no corpo da requisição 
     //para a rota da api que faz o login /auth
     //lembre-se que essa rota vai retornar um Bearer Token e o mesmo deve ser salvo
     //no localstorage para ser usado em chamadas futuras
     //Com tudo ocorrendo corretamente, o usuário deve ser redirecionado a página principal,com react-router
     //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
+    try {
+      const {data} = await api.post("/auth",{ 
+        username,
+        password,
+    })
+      navigate('/home')
+    } catch (error) {
+      alert("Dados Invalidos")
+    }
   };
 
   return (
@@ -25,6 +43,7 @@ const LoginForm = () => {
               placeholder="Login"
               name="login"
               required
+              onChange={(event)=>setUsername(event.target.value)}
             />
             <input
               className={`form-control ${styles.inputSpacing}`}
@@ -32,6 +51,7 @@ const LoginForm = () => {
               name="password"
               type="password"
               required
+              onChange={(event)=>setPassorwd(event.target.value)}
             />
             <button className="btn btn-primary" type="submit">
               Send
