4cab97878eff5ecf667281bc52b10fea0b4aeb31
GustavonBrito <gustavoserafio@gmail.com>
2023-09-19 20:38:30 -0300

    Retornando ao original

--- src/tests/test-utils.js
+++ src/tests/test-utils.js
@@ -10,7 +10,7 @@ import App from "../App"
 const renderWithContext = (ui, providerValue)=>{
     return render(
         <BrowserRouter>
-            <ContextProvider value={providerValue || {theme: "liaght", data: []}} >   
+            <ContextProvider value={providerValue || {theme: "light", data: []}} >   
                 {ui}
             </ContextProvider>
         </BrowserRouter>
