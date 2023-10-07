6e8fdddd74b4debc1828c6c7a6457e56b139d23d
Edmundo Batista <eddmundosjb@gmail.com>
2023-10-07 18:57:56 -0300

    feat: atualizando a develop

--- src/Components/ScheduleForm/ScheduleForm.jsx
+++ src/Components/ScheduleForm/ScheduleForm.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect} from "react";
 import styles from "./ScheduleForm.module.css";
 import { ContextGlobal } from "../../contexts/global.context";
 
